794250796e996a1888945d15db56678a53b4657f
Iqbal Fasri <iqbalx64@gmail.com>
2019-01-15 14:11:21 +0700

    Updates

---
+++ precache-manifest.b0677046a88b45f8dd6493134dfe571f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3df27bc8b13a61f0c1c262a7842b3be1",
+    "url": "/quranid/static/media/404ilus.3df27bc8.png"
+  },
+  {
+    "revision": "4b4d8e751131ab34af48",
+    "url": "/quranid/static/js/runtime~main.4b4d8e75.js"
+  },
+  {
+    "revision": "f0f92d7171c0b6eefdc1",
+    "url": "/quranid/static/js/main.f0f92d71.chunk.js"
+  },
+  {
+    "revision": "84b6c93e9e04bbb86ce0",
+    "url": "/quranid/static/js/1.84b6c93e.chunk.js"
+  },
+  {
+    "revision": "f0f92d7171c0b6eefdc1",
+    "url": "/quranid/static/css/main.48cf2999.chunk.css"
+  },
+  {
+    "revision": "e63830d6a853e7e682591829b3cc3d63",
+    "url": "/quranid/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e52516b6480f0214654c563767cc5a77.js"
+  "/quranid/precache-manifest.b0677046a88b45f8dd6493134dfe571f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/quranid/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
